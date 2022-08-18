79d42470f7b21625eeb4ab66bd3c145cc40b2559
mhedtman <mhedtman@student.42heilbronn.de>
2022-08-18 09:05:20 +0200

    gitignore added and env removed

--- src/parser.c
+++ src/parser.c
@@ -3,21 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: mhedtman <mhedtman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 20:50:16 by pgeeser           #+#    #+#             */
-/*   Updated: 2022/08/17 21:38:49 by pgeeser          ###   ########.fr       */
+/*   Updated: 2022/08/18 08:59:24 by mhedtman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	**parse_cmds(char **cmd_arr)
+{
+	
+}
 
 void	parse_input(char *input)
 {
 	char	**array;
 
 	g_minishell.cmd_array = ft_split(input, ' ');
-	array = g_minishell.cmd_array;
+	array = parse_cmds(g_minishell.cmd_array);
 	while (*array)
 	{
 		builtin_parser(array);
