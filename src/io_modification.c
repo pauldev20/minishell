277ef9925b0b8ac4332413cb8dc95c62e4236a1b
mhedtman <mhedtman@student.42heilbronn.de>
2022-10-19 17:07:46 +0200

    io

--- src/io_modification.c
+++ src/io_modification.c
@@ -6,7 +6,7 @@
 /*   By: mhedtman <mhedtman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:14:26 by mhedtman          #+#    #+#             */
-/*   Updated: 2022/10/19 10:25:58 by mhedtman         ###   ########.fr       */
+/*   Updated: 2022/10/19 16:37:11 by mhedtman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,13 @@ int	get_outfile_fd(char *token, char *arr, int pipe)
 	int	fd;
 
 	fd = STDOUT_FILENO;
+	printf("[token %s] [arr %s]\n", token, arr);
 	if (!token || !arr)
 		return (pipe);
 	else if (str_is_equal(token, "GREAT"))
-		fd = open(arr, O_WRONLY | O_CREAT | O_TRUNC, 00700);
+		fd = open(arr, O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 00700);
 	else if (str_is_equal(token, "DGREAT"))
-		fd = open(arr, O_RDWR | O_APPEND | O_CREAT, 00700);
+		fd = open(arr, O_RDWR | O_APPEND | O_CREAT | O_CLOEXEC, 00700);
 	if (fd == -1)
 		print_error(2, NULL, 1);
 	return (fd);
@@ -52,7 +53,7 @@ int	get_infile_fd(t_ct *cmd_table, char *token, char *arr, int pipe)
 	if (!token || !arr)
 		return (pipe);
 	else if (str_is_equal(token, "LESS"))
-		fd = open(arr, O_RDONLY, 0777);
+		fd = open(arr, O_RDONLY | O_CLOEXEC, 0777);
 	else if (str_is_equal(token, "DLESS"))
 	{
 		if (here_doc == 0)
@@ -60,7 +61,7 @@ int	get_infile_fd(t_ct *cmd_table, char *token, char *arr, int pipe)
 			here_doc_execute(cmd_table);
 			here_doc = 1;
 		}
-		fd = open("/tmp/here_doc", O_RDONLY, 0777);
+		fd = open("/tmp/here_doc", O_RDONLY | O_CLOEXEC, 0777);
 		if (fd == -1)
 			print_error(2, NULL, 0);
 		return (fd);
